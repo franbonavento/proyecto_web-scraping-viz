194d2d89b005d515cf04b5a35411d836eb03b132
Franco Bonavento <franbonavento@gmail.com>
2025-04-23 15:47:58 -0300

    estructurar en carpetas
    modif1

--- SQLQuery_webscraping_ryzen.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE PrecioComponentes (
-    ID INT IDENTITY(1,1) PRIMARY KEY,
-    Producto VARCHAR(255),
-    Precio DECIMAL(10, 2),
-    FechaHora DATETIME
-);
-
-
-select * from PrecioComponentes
